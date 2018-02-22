bed2a8046a3cb21895ee0ff33cfb6435613df0ca
Fabian Schlebusch <fabian.schlebusch@rwth-aachen.de>
2018-02-22 17:23:02 +0100

    Changed naming convention for variables to avoid name confusion #1

--- FindHPCPatternConsumer.cpp
+++ FindHPCPatternConsumer.cpp
@@ -10,30 +10,31 @@
 
 
 
-void FindHPCPatternConsumer::HandleTranslationUnit(clang::ASTContext &Context) 
+void FindHPCPatternConsumer::HandleTranslationUnit(clang::ASTContext &Ctxt) 
 {
 	/* Traverse the AST for comments and parse them */
 	DEBUG_MESSAGE("Using Visitor to traverse from top translation declaration unit");
-	Visitor.TraverseDecl(Context.getTranslationUnitDecl());
+	Visitor.TraverseDecl(Ctxt.getTranslationUnitDecl());
 
 	/* Print raw comment list for checking */
 #ifdef PRINT_DEBUG
-	clang::SourceManager &SrcManager = Context.getSourceManager();
-	clang::RawCommentList &RawCommentList = Context.getRawCommentList();
+	clang::SourceManager& SrcMgr = Ctxt.getSourceManager();
 
-	llvm::ArrayRef<clang::RawComment*> RawComments = RawCommentList.getComments();
-	llvm::ArrayRef<clang::RawComment*>::iterator RawComment;
-
-	SrcManager.dump();
-
-	std::cout << "List of raw comments contains " << RawComments.size() << " entries." << std::endl;
+	clang::RawCommentList &RCmtLst = Ctxt.getRawCommentList();
+	llvm::ArrayRef<clang::RawComment*> RCmts = RCmtLst.getComments();
 	
-	std::cout << "Now printing list of raw comments:" << std::endl;
-
-	for (RawComment = RawComments.begin(); RawComment != RawComments.end(); RawComment++)
+	if (!RCmts.empty())
 	{
-		llvm::StringRef RawText = (*RawComment)->getRawText(SrcManager);
-		std::cout << RawText.str() << std::endl;
+		llvm::ArrayRef<clang::RawComment*>::iterator RCmt;
+
+		Decl->dump();
+		std::cout << "List of raw comments attachted to this declaration:" << std::endl;
+
+		for (RCmt = RCmts.begin(); RCmt != RCmts.end(); RCmt++)
+		{
+			llvm::StringRef RTxt = (*RCmt)->getRawText(SrcMgr);
+			std::cout << RTxt.str() << std::endl;
+		}
 	}
 #endif
 }
