c96556a95be36e071a1b5bbcb2209afd2219c904
Fabian Schlebusch <fabian.schlebusch@rwth-aachen.de>
2018-04-10 11:22:36 +0200

    Added Handler for Pattern End function

--- HPCPatternInstrHandler.cpp
+++ HPCPatternInstrHandler.cpp
@@ -3,11 +3,21 @@
 
 #include <iostream>
 
-void HPCPatternInstrHandler::run(const clang::ast_matchers::MatchFinder::MatchResult &Result)
+void HPCPatternBeginInstrHandler::run(const clang::ast_matchers::MatchFinder::MatchResult &Result)
 {
 	const clang::StringLiteral* patternstr = Result.Nodes.getNodeAs<clang::StringLiteral>("patternstr");	
 	std::cout << patternstr->getString().str() << std::endl;
 
 	HPCParallelPattern pattern(patternstr->getString().str());
+#if PRINT_DEBUG
 	pattern.Print();	
+#endif
+}
+
+void HPCPatternEndInstrHandler::run(const clang::ast_matchers::MatchFinder::MatchResult &Result)
+{
+	const clang::StringLiteral* patternstr = Result.Nodes.getNodeAs<clang::StringLiteral>("patternstr");	
+	std::cout << patternstr->getString().str() << std::endl;
+
+	RemoveFromPatternStack(patternstr->getString().str());
 }
--- HPCPatternInstrHandler.h
+++ HPCPatternInstrHandler.h
@@ -4,7 +4,16 @@
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
 
-class HPCPatternInstrHandler : public clang::ast_matchers::MatchFinder::MatchCallback 
+
+class HPCPatternBeginInstrHandler : public clang::ast_matchers::MatchFinder::MatchCallback 
+{
+public:
+	virtual void run (const clang::ast_matchers::MatchFinder::MatchResult &Result);
+};
+
+
+
+class HPCPatternEndInstrHandler : public clang::ast_matchers::MatchFinder::MatchCallback
 {
 public:
 	virtual void run (const clang::ast_matchers::MatchFinder::MatchResult &Result);
