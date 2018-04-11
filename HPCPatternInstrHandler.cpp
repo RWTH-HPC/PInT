024d3a5e5a40301821989b5072a30d3bf59156d6
Fabian Schlebusch <fabian.schlebusch@rwth-aachen.de>
2018-04-11 16:48:24 +0200

    Pattern Handler now adds the pattern to the stack and does not output the name anymore

--- HPCPatternInstrHandler.cpp
+++ HPCPatternInstrHandler.cpp
@@ -13,10 +13,11 @@ void HPCPatternBeginInstrHandler::SetCurrentFnEntry(FunctionDeclDatabaseEntry* F
 void HPCPatternBeginInstrHandler::run(const clang::ast_matchers::MatchFinder::MatchResult &Result)
 {
 	const clang::StringLiteral* patternstr = Result.Nodes.getNodeAs<clang::StringLiteral>("patternstr");	
-	std::cout << patternstr->getString().str() << std::endl;
-
+	
 	HPCParallelPattern* Pattern = new HPCParallelPattern(patternstr->getString().str());
-	CurrentFnEntry->Patterns.push_back(Pattern);
+	CurrentFnEntry->AddPattern(Pattern);
+
+	AddToPatternStack(Pattern);
 
 #if PRINT_DEBUG
 	Pattern->Print();	
