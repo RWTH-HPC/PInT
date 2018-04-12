354e4a4c39c3d04db23d832c8f09c5f8b60c754e
Fabian Schlebusch <fabian.schlebusch@rwth-aachen.de>
2018-04-12 11:09:41 +0200

    Patterns are now only added to FnEntry if they are not contained in another pattern

--- HPCPatternInstrHandler.cpp
+++ HPCPatternInstrHandler.cpp
@@ -15,7 +15,11 @@ void HPCPatternBeginInstrHandler::run(const clang::ast_matchers::MatchFinder::Ma
 	const clang::StringLiteral* patternstr = Result.Nodes.getNodeAs<clang::StringLiteral>("patternstr");	
 	
 	HPCParallelPattern* Pattern = new HPCParallelPattern(patternstr->getString().str());
-	CurrentFnEntry->AddPattern(Pattern);
+	
+	if (GetTopPatternStack() == NULL)
+	{	
+		CurrentFnEntry->AddPattern(Pattern);
+	}
 
 	AddToPatternStack(Pattern);
 
@@ -29,7 +33,6 @@ void HPCPatternBeginInstrHandler::run(const clang::ast_matchers::MatchFinder::Ma
 void HPCPatternEndInstrHandler::run(const clang::ast_matchers::MatchFinder::MatchResult &Result)
 {
 	const clang::StringLiteral* patternstr = Result.Nodes.getNodeAs<clang::StringLiteral>("patternstr");	
-	std::cout << patternstr->getString().str() << std::endl;
 
 	RemoveFromPatternStack(patternstr->getString().str());
 }
