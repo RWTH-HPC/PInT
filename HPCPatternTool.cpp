bd7ae720a7be2ca7e13a8470b88d3c3f4324dde8
Esther <tala@itc.rwth-aachen.de>
2019-07-05 11:31:20 +0200

    Eddited the display of -pintVersion

--- HPCPatternTool.cpp
+++ HPCPatternTool.cpp
@@ -93,7 +93,7 @@ int main (int argc, const char** argv)
 		clang::tooling::CommonOptionsParser OptsParserVersion(argc, argv, pintVersion);
 
 	if(PintVersion.getValue()){
-			std::cout << "You currently using Version: " << PInTVersion <<'\n';
+			std::cout << "You currently using Version " << PInTVersion <<" of the PInT"<<'\n';
 	}
 	else{
 		clang::tooling::CommonOptionsParser OptsParser(argc, argv, HPCPatternToolCategory);
